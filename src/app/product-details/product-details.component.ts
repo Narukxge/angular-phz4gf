1b7cad260a0cbd30be2a542ed67af9b28d88bbf6
Narukxge <93284922+Narukxge@users.noreply.github.com>
2021-11-10 12:48:49 +0100

    Started Using Visual Studio Code

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products} from '../products';
+
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  
+  product: Product | undefined;
 
-  constructor() { }
+  constructor(private rout: ActivatedRoute) {
 
-  ngOnInit(): void {
+
+   }
+   
+  ngOnInit() {
+      
+      const routeParams = this.rout.snapshot.paramMap;
+      const productIdFromRoute = Number(routeParams.get('productId'));
+
+      this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
