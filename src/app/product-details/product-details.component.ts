a0fb58b25f6e951448b4a4727402036e600fd33a
Narukxge <93284922+Narukxge@users.noreply.github.com>
2021-11-11 08:33:47 +0100

    Added new Featuers

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products} from '../products';
-
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,9 +12,14 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor(private rout: ActivatedRoute) {
+  constructor(private rout: ActivatedRoute, private cartService: CartService) {
+  
 
+   }
 
+   addToCart(product: Product) {
+      this.cartService.addToCart(product);
+      window.alert("Your Product added to Cart!");
    }
    
   ngOnInit() {
